9fc4e1d973aafa33b23cfe45732688cd172d73b4
kw-buchi <kwbuchi1964@gmail.com>
2025-06-13 18:33:49 +0900

    ５回にする

--- test2/src/test2/Test1.java
+++ test2/src/test2/Test1.java
@@ -5,6 +5,10 @@ public class Test1 {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		System.out.println("Hello");
+		System.out.println("Hello");
+		System.out.println("Hello");
+		System.out.println("Hello");
+		System.out.println("Hello");
 	}
 
 }
