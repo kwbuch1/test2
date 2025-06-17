5bef2ed04cf4b82363396ad7fbd0491a8ba9241b
kwbuchi <hiroyuki.kawabuchi1@akkodis.co.jp>
2025-06-17 10:20:55 +0900

    セッター・ゲッター追加

--- test2/src/test2/Test3.java
+++ test2/src/test2/Test3.java
@@ -4,8 +4,17 @@ public class Test3 {
 
 	int value1;
 	String str1;
-	
-	
-	
+	public int getValue1() {
+		return value1;
+	}
+	public void setValue1(int value1) {
+		this.value1 = value1;
+	}
+	public String getStr1() {
+		return str1;
+	}
+	public void setStr1(String str1) {
+		this.str1 = str1;
+	}
 	
 }
